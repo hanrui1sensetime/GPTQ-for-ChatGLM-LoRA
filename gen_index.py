8dca83bf8329378f32a22985f6820b60f582b146
hanrui1sensetime <hanrui1@sensetime.com>
2023-05-30 04:07:20 +0000

    add gen_index.py

---
+++ gen_index.py
@@ -0,0 +1,14 @@
+import json
+import os
+import torch
+
+ckpt = '/root/workspace/external_data/PJ-LLaMa-13B/PJ-LLaMa-13B-int4-triton.bin'
+model = torch.load(ckpt)
+jsondict = dict(metadata=dict(total_size=os.path.getsize(ckpt)))
+jsondict['weight_map'] = {}
+for key in model.keys():
+    jsondict['weight_map'][key] = ckpt.split('/')[-1]
+
+with open('pytorch_model.bin.index.json', 'w+') as f:
+    json.dump(jsondict, f)
+
